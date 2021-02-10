408dbb6ffbddb099969cf683691913ff74a3eb4d
Surfacing600 <74630359+Surfacing600@users.noreply.github.com>
2021-02-10 23:15:30 +0000

    changed to sqlite3

--- app.py
+++ app.py
@@ -10,28 +10,24 @@ app.config['SECRET_KEY'] = b'a~\x95EJ\x08\x8b\x97\xbc\xbb\x05\x1a\\b\xf5\xe1\xf5
 @app.teardown_appcontext
 
 def close_db(error):#check if there is active database and if there is it will close it
-    
-    if hasattr(g, 'postgres_db_cur'):
-        g.postgres_db_cur.close()
-    
-    if hasattr(g, 'postgres_db_conn'):
-        g.postgres_db_conn.close()
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def rota():
 
     db = get_db()#initialise the database
-    db.execute('select * from tenant1')
-    name1 = db.fetchall()
-    db.execute('select * from tenant2')
-    name2 = db.fetchall()
-    db.execute('select * from tenant3')
-    name3 = db.fetchall()
-    db.execute('select * from tenant4')
-    name4 = db.fetchall()
-    db.execute('select * from rubbish_removal')
-    rubbish = db.fetchall()
+    cursor1 = db.execute('select * from tenant1')
+    name1 = cursor1.fetchall()
+    cursor2 = db.execute('select * from tenant2')
+    name2 = cursor2.fetchall()
+    cursor3 = db.execute('select * from tenant3')
+    name3 = cursor3.fetchall()
+    cursor4 = db.execute('select * from tenant4')
+    name4 = cursor4.fetchall()
+    cursor5 = db.execute('select * from rubbish_removal')
+    rubbish = cursor5.fetchall()
     
 
     date1 = date.today()+ timedelta(days=11)
@@ -44,16 +40,16 @@ def rota():
 def update():
 
     db = get_db()#initialise the database
-    db.execute('select * from tenant1')
-    tenant1 = db.fetchall()
-    db.execute('select * from tenant2')
-    tenant2 = db.fetchall()
-    db.execute('select * from tenant3')
-    tenant3 = db.fetchall()
-    db.execute('select * from tenant4')
-    tenant4 = db.fetchall()
-    db.execute('select * from rubbish_removal')
-    rubbish = db.fetchall()
+    cursor1 = db.execute('select * from tenant1')
+    tenant1 = cursor1.fetchall()
+    cursor2 = db.execute('select * from tenant2')
+    tenant2 = cursor2.fetchall()
+    cursor3 = db.execute('select * from tenant3')
+    tenant3 = cursor3.fetchall()
+    cursor4 = db.execute('select * from tenant4')
+    tenant4 = cursor4.fetchall()
+    cursor5 = db.execute('select * from rubbish_removal')
+    rubbish = cursor5.fetchall()
 
     if request.method == 'POST':
 
@@ -68,18 +64,13 @@ def update1(id):
     db = get_db()#initialise the database
     if request.method == 'POST':
         try:
-            db.execute('select id, tenant_name1 from tenant1 where id = %s', (id, ))
-            tenant_to_update = db.fetchone()
-            name1 = request.form['tenant_1']
-            db.execute('update tenant1 set tenant_name1 = %s where id = %s', (name1, id))
+            
+            db.execute('update tenant1 set tenant_name1 = ? where id = ?', [request.form['tenant_1'], id])
             db.commit()
             return redirect(url_for('update'))
         except:
 
-            db.execute('select id, tenant_name2 from tenant2 where id = %s', (id, ))
-            tenant_to_update = db.fetchone()
-            name2 = request.form['tenant_2']
-            db.execute('update tenant2 set tenant_name2 = %s where id = %s', (name2, id))
+            db.execute('update tenant2 set tenant_name2 = ? where id = ?', [request.form['tenant_2'], id])
             db.commit()
             return redirect(url_for('update'))
     return render_template('update.html')
@@ -90,31 +81,28 @@ def update2(id):
     db = get_db()#initialise the database
     if request.method == 'POST':
         try:
-            db.execute('select id, tenant_name3 from tenant3 where id = %s', (id, ))
-            tenant_to_update = db.fetchone()
-            name3 = request.form['tenant_3']
-            db.execute('update tenant3 set tenant_name3 = %s where id = %s', (name3, id))
+            
+ 
+            db.execute('update tenant3 set tenant_name3 = ? where id = ?', [request.form['tenant_3'], id])
             db.commit()
             return redirect(url_for('update'))
         except:
 
-            db.execute('select id, tenant_name4 from tenant4 where id = %s', (id, ))
-            tenant_to_update = db.fetchone()
-            name4 = request.form['tenant_4']
-            db.execute('update tenant4 set tenant_name4 = %s where id = %s', (name4, id))
+            
+            db.execute('update tenant4 set tenant_name4 = ? where id = ?', [request.form['tenant_4'], id])
             db.commit()
             return redirect(url_for('update'))
-    return render_template('update.html')
+    return render_template('/update2/<id>')
 
 @app.route('/update3/<id>', methods=['GET', 'POST'])
 def update3(id):
 
     db = get_db()#initialise the database
     if request.method == 'POST':
-            db.execute('select id, tenant_rubbish from rubbish_removal where id = %s', (id, ))
-            tenant_to_update = db.fetchall()
+            
+
             name5 = request.form['rubbish']
-            db.execute('update rubbish_removal set tenant_rubbish = %s where id = %s', (name5, id))
+            db.execute('update rubbish_removal set tenant_rubbish = ? where id = ?', [name5, id])
             db.commit()
             return redirect(url_for('update'))
     return render_template('update.html')
--- schema.sql
+++ schema.sql
@@ -1,24 +1,24 @@
 create table tenant1(
-    id integer primary key,
+    id integer primary key autoincrement,
     tenant_name1 text not null
 );
 
 create table tenant2(
-    id serial primary key,
+    id integer primary key autoincrement,
     tenant_name2 text not null
 );
 
 create table tenant3(
-    id serial primary key,
+    id integer primary key autoincrement,
     tenant_name3 text not null
 );
 
 create table tenant4(
-    id serial primary key,
+    id integer primary key autoincrement,
     tenant_name4 text not null
 );
 
 create table rubbish_removal(
-    id serial primary key,
+    id integer primary key autoincrement,
     tenant_rubbish text not null
 );
