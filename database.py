5693200e21ad30e97364b98ff4db72b44ba93f87
Surfacing600 <74630359+Surfacing600@users.noreply.github.com>
2021-02-11 19:21:41 +0000

    postgres version

--- database.py
+++ database.py
@@ -1,15 +1,29 @@
 from flask import g
-import sqlite3
+import psycopg2
+from psycopg2.extras import DictCursor
 
-
-def connect_db(): #connects to the database
-    sql = sqlite3.connect('/Users/Dmitry/Desktop/electronic_rota/database.db')
-    sql.row_factory = sqlite3.Row #to get dictionaries instead of tuples when you access the rows
-    return sql
-
+def connect_db():
+    conn = psycopg2.connect('postgres://jspcxodfhzzyhf:390459475d7d8fe4ed4825fa3cb45fc36e035665762122112cee83a90e1d2e9e@ec2-3-222-11-129.compute-1.amazonaws.com:5432/d8gn9j0gp84qm0', cursor_factory=DictCursor)
+    conn.autocommit = True
+    sql = conn.cursor()
+    return conn, sql
 
 def get_db():
-    if not hasattr(g, 'sqlite3_db'):#checks if global object 'sqlite3_db' is in the database
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+    db = connect_db()
+
+    if not hasattr(g, 'postgres_db_conn'):
+        g.postgres_db_conn = db[0]
+
+    if not hasattr(g, 'postgres_db_cur'):
+        g.postgres_db_cur = db[1]
+
+    return g.postgres_db_cur
+
+def init_db():
+    db = connect_db()
+
+    db[1].execute(open('schema.sql', 'r').read())
+    db[1].close()
+
+    db[0].close()
 
